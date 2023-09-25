dce11faba2d8d57136d6d734e958cfd9cb722aaf
YangSiJun528 <yangsijun5528@gmail.com>
2023-09-25 09:58:20 +0900

    :art: GlobalSocketExceptionHandler 구조 변경

--- src/main/kotlin/com/gsmNetworking/chat/global/exception/handler/GlobalSocketExceptionHandler.kt
+++ src/main/kotlin/com/gsmNetworking/chat/global/exception/handler/GlobalSocketExceptionHandler.kt
@@ -1,34 +1,29 @@
 package com.gsmNetworking.chat.global.exception.handler
 
-import com.gsmNetworking.chat.common.error.exception.StompException
-import com.gsmNetworking.chat.common.socket.processor.StompErrorProcessor
+import com.gsmNetworking.chat.domain.user.exception.UserStompException
 import org.slf4j.LoggerFactory
-import org.springframework.messaging.Message
-import org.springframework.stereotype.Component
-import org.springframework.web.socket.messaging.StompSubProtocolErrorHandler
+import org.springframework.messaging.handler.annotation.MessageExceptionHandler
+import org.springframework.web.bind.annotation.RestControllerAdvice
 
-
-/**
- * Socket 통신 중에 발생하는 에러를 전역적으로 관리하는 handler 입니다.
- */
-@Component
-class GlobalSocketExceptionHandler(
-    private val stompErrorProcessor: StompErrorProcessor
-) : StompSubProtocolErrorHandler() {
+@RestControllerAdvice
+class GlobalSocketExceptionHandler {
     private val log = LoggerFactory.getLogger(this.javaClass)!! //TODO 로깅 구현 통일하기
-    override fun handleClientMessageProcessingError(
-        clientMessage: Message<ByteArray>?,
-        ex: Throwable
-    ): Message<ByteArray>? {
-        when (val cause = ex.cause) {
-            is StompException -> {
-                stompErrorProcessor.sendError(cause)
-            }
-            is Exception -> {
-                log.error("An error occurred", ex)
-            }
-        }
 
-        return super.handleClientMessageProcessingError(clientMessage, ex)
-    }
+    /**
+     * [UserStompException] 혹은 해당 클래스의 자식을 핸들링하는 메서드 입니다.
+     *
+     * @param e [UserStompException] 혹은 [UserStompException]을 상속하는 클래스
+     */
+    @MessageExceptionHandler(UserStompException::class)
+    fun handler(e: UserStompException): Unit = TODO("send error to client")
+
+
+    /**
+     * 예외처리하지 않은 [RuntimeException] 포함한 하위 에러를 핸들링합니다.
+     *
+     * @param e [RuntimeException]또는 [RuntimeException]을 상속하는 객체
+     */
+    @MessageExceptionHandler(Exception::class)
+    fun handler(e: Exception): Unit = log.error("Uncatched Exception", e)
+
 }
