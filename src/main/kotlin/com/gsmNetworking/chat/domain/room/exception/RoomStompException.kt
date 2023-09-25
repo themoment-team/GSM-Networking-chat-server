80c47546e91b88fc7de5bf2ce0b0544086db958c
YangSiJun528 <yangsijun5528@gmail.com>
2023-09-25 09:56:09 +0900

    :art: RoomStompException에 누락된 userId 필드 추가

--- src/main/kotlin/com/gsmNetworking/chat/domain/room/exception/RoomStompException.kt
+++ src/main/kotlin/com/gsmNetworking/chat/domain/room/exception/RoomStompException.kt
@@ -2,6 +2,7 @@ package com.gsmNetworking.chat.domain.room.exception
 
 import com.gsmNetworking.chat.common.error.exception.StompException
 import com.gsmNetworking.chat.common.error.model.ErrorCode
+import java.util.UUID
 
 /**
  * [StompException]의 구현체, Room 도메인에서 발생한 예외에 대한 설명을 Client에게 전달하기 위해 사용합니다.
@@ -11,6 +12,7 @@ import com.gsmNetworking.chat.common.error.model.ErrorCode
  */
 class RoomStompException(
     code: ErrorCode = ErrorCode.DEFAULT,
-    message: String
+    message: String,
+    val userId: UUID
 ) : StompException(code, message) {
 }
