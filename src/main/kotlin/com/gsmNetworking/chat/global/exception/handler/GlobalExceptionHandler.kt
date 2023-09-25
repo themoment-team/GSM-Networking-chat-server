f16139ad6ec839adab96c3043ca810bf08c7a90a
YangSiJun528 <yangsijun5528@gmail.com>
2023-09-25 09:59:16 +0900

    :sparkles: GlobalExceptionHandler 예외처리 하지 않은 Exception Handling 기능 추가

--- src/main/kotlin/com/gsmNetworking/chat/global/exception/handler/GlobalExceptionHandler.kt
+++ src/main/kotlin/com/gsmNetworking/chat/global/exception/handler/GlobalExceptionHandler.kt
@@ -2,12 +2,14 @@ package com.gsmNetworking.chat.global.exception.handler
 
 import com.gsmNetworking.chat.common.error.exception.ExpectedException
 import com.gsmNetworking.chat.common.error.model.ExceptionResponse
+import org.slf4j.LoggerFactory
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
 
 @RestControllerAdvice
 class GlobalExceptionHandler {
+    private val log = LoggerFactory.getLogger(this.javaClass)!! //TODO 로깅 구현 통일하기
 
     /**
      * ExpectedException과 ExpectedException을 상속받은 예외 클래스를 핸들해는 메서드 입니다.
@@ -21,4 +23,12 @@ class GlobalExceptionHandler {
             .status(e.status)
             .body(ExceptionResponse(message = e.message))
 
+
+    /**
+     * 예외처리하지 않은 [RuntimeException] 포함한 하위 에러를 핸들링합니다.
+     *
+     * @param e [RuntimeException]또는 [RuntimeException]을 상속하는 객체
+     */
+    @ExceptionHandler(Exception::class)
+    fun handler(e: Exception): Unit = log.error("Uncatched Exception", e)
 }
