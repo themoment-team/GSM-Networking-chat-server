4d665e3f2a2bd028989502824f7d35298819cadc
YangSiJun528 <yangsijun5528@gmail.com>
2023-09-25 10:37:33 +0900

    :art: StompException 추상클래스로 변경

--- src/main/kotlin/com/gsmNetworking/chat/common/error/exception/StompException.kt
+++ src/main/kotlin/com/gsmNetworking/chat/common/error/exception/StompException.kt
@@ -6,7 +6,7 @@ import com.gsmNetworking.chat.common.error.model.ErrorCode
  * GSM Networking 프로젝트에서 사용하는 Exception,
  * STOMP 통신 중인 사용자에게 예외 상황을 알리기 위해 사용합니다.
  */
-open class StompException(
+abstract class StompException(
     val code: ErrorCode = ErrorCode.DEFAULT,
     override val message: String
 ): RuntimeException(message)
